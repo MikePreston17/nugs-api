afe51cc58710b08eab290ceb97bc8a1e02b26fe9
MikePreston17 <michael.n.preston@gmail.com>
2021-06-21 01:27:52 -0500

    Fixed server issue where returns were not returning due to schema constraints

--- api/server.js
+++ api/server.js
@@ -83,35 +83,34 @@ fastify.route({
 fastify.route({
     method: 'GET',
     url: '/',
-    schema: {
-        // request needs to have a querystring with a `name` parameter
-        querystring: {
-            name: { type: 'string' }
-        },
-        // the response needs to be an object with an `hello` property of type 'string'
-        response: {
-            200: {
-                type: 'object',
-                properties: {
-                    hello: { type: 'string' }
-                }
-            }
-        }
-    },
+    // schema: {
+    //     // request needs to have a querystring with a `name` parameter
+    //     querystring: {
+    //         name: { type: 'string' }
+    //     },
+    //     // the response needs to be an object with an `hello` property of type 'string'
+    //     response: {
+    //         200: {
+    //             type: 'object',
+    //             // properties: {
+    //             //     hello: { type: 'string' }
+    //             // }
+    //         }
+    //     }
+    // },
     // this function is executed for every request before the handler is executed
-    preHandler: async (request, reply) => {
-        // E.g. check authentication
-    },
+    // preHandler: async (request, reply) => {
+    //     // E.g. check authentication
+    // },
     handler: async (request, reply) => {
         let query = `MATCH (n) return n LIMIT 25`
         let result = await session.run(query)
         const single = result.records[0];
         const node = single.get(0)
 
-        console.log(`result.records`, result.records)
-        console.log(`single`, single)
-        console.log(`node`, node.properties.name)
-        return firearms
+        let nugs = result.records.map(n => n.get(0).properties)
+        dev && console.log(`nugs`, nugs)
+        return nugs
     }
 })
 
